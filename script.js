e42afc5ac8474a8d09055c68625d0c22207c4a4b
Nxsh <nathanael.mutua.m@gmail.com>
2025-05-27 19:31:14 +0300

    add javascript logic to fetch data
    - add findMostProbableCountry function to get extract the country
    probabilities using a for loop.
    - add function to convert the country id values to country names

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const nameInput = document.getElementById("name-input");
 const submitSearchBtn = document.getElementById("submit-search-btn");
 const searchInformation = document.querySelector(".search-information");
 
+function findMostProbableCountry(object){
+    let maxProbability = 0;
+    let mostProbableCountry = null;
+    for (let i = 0; i < object.length; i++) {
+      let countryObject = object[i];
+      if (countryObject.probability > maxProbability) {
+      maxProbability = countryObject.probability;
+      mostProbableCountry = countryObject;
+      }
+  }
+  return mostProbableCountry
+}
+
 submitSearchBtn.addEventListener("click", async (e) => {
   let searchName = nameInput.value;
   if (searchName === "") {
@@ -13,5 +26,15 @@ submitSearchBtn.addEventListener("click", async (e) => {
     `https://api.nationalize.io/?name=${searchName}`,
   );
   const outputData = await response.json();
-  console.log(outputData);
+  const {name, country} = outputData;
+
+  const probableCountry = findMostProbableCountry(country);
+
+  let probableCountryValue = probableCountry.country_id;
+  const countryProbability = (Math.trunc(probableCountry.probability*1000))/10;
+
+  let probableCountryName = new Intl.DisplayNames(['en'], {type: 'region'});
+  const countryName = probableCountryName.of(probableCountryValue);  // gives the full name
+
+  searchInformation.innerHTML = `<span class="found-result">${name} is from ${countryName} with ${countryProbability}% certainty</span>`
 });
